1b9ed3a76a665f84fcf7a86e7593b312adb57e08
Michalina Hansdorfer <hansdorfer.m@gmail.com>
2021-01-15 12:36:27 +0100

    Added leaflet, react-leaflet and showed location on a map.

--- src/components/LocationMap.js
+++ src/components/LocationMap.js
@@ -1,5 +1,19 @@
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import "../css/LocationMap.css";
+
 export default function LocationMap(props){
     const {latitude, longitude} = props;
-
-    return <div>latitude: {latitude}, longitude: {longitude} </div>
+    
+    return (latitude && longitude) ? 
+            <MapContainer center={[latitude, longitude]} zoom={13} scrollWheelZoom={false}>
+                <TileLayer
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={[latitude, longitude]}>
+                <Popup>
+                    You're here!
+                </Popup>
+                </Marker>
+            </MapContainer> : null;
 }
