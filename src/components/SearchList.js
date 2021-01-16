d2e61fcd225079efa2727113ebe818ff70cf7ca5
Michalina Hansdorfer <hansdorfer.m@gmail.com>
2021-01-16 16:12:25 +0100

    Show different text if nothing was searched yet

--- src/components/SearchList.js
+++ src/components/SearchList.js
@@ -2,13 +2,15 @@ import ListGroup from 'react-bootstrap/ListGroup';
 
 export default function SearchList(props){
     return (
-        <>
-            <h5>Recently you have searched:</h5>
-            <ListGroup className="text-dark overflow-auto" style={{height: "86vh"}}>
-                {props.locations ? props.locations.map((location, index)=>{
-                    return <ListGroup.Item key={location + "-" +index}>{location}</ListGroup.Item> 
-                }) : null}
-            </ListGroup>
-        </>
+        props.locations && props.locations.length > 0 ?
+            <>
+                <h5>Recently you have searched for:</h5>
+                <ListGroup className="text-dark overflow-auto mt-3" style={{height: "86vh"}}>
+                    {props.locations ? props.locations.map((location, index)=>{
+                        return <ListGroup.Item key={location + "-" +index}>{location}</ListGroup.Item> 
+                    }) : null}
+                </ListGroup>
+            </>
+            : <h5 className="text-center mt-2">Search list will appear here</h5>
     );
 }
