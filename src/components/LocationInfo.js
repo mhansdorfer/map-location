1b09dd2ce8b8c219ee8cb63df75475629277814d
Michalina Hansdorfer <hansdorfer.m@gmail.com>
2021-01-16 19:59:26 +0100

    Added border to location info

--- src/components/LocationInfo.js
+++ src/components/LocationInfo.js
@@ -1,10 +1,16 @@
+import "../css/LocationMap.css";
+
 export default function LocationInfo(props){
     const {country_name, city, ip} = props;
-    return (
-        <section className="text-center py-5">
-            <h3>{city}</h3>
-            <h5>{country_name}</h5>
-            <p>{ip}</p> 
-        </section>
+    return ( 
+        <>
+            { ip && 
+                <section className="text-center py-5 border border-light location-info">
+                    <h3>{city}</h3>
+                    <h5>{country_name}</h5>
+                    <p>{ip}</p> 
+                </section>
+            }
+        </>
     );
 }
